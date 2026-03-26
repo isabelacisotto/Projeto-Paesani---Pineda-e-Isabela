918059cd2a02905b0d72f37815f63138598bc47c
vpineda30 <vinicius.pineda.senai404@gmail.com>
2026-03-26 10:42:59 -0300

    feat: Set up the initial React project structure with Vite, including core components, styling, and configuration files.

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+export function Header() {
+    return(
+        <>
+            <header>
+                <div className="logo">
+                    <img src="" alt="" />
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Produtos</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
